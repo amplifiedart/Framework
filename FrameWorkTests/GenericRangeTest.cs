4f243c41c2a168668306f9a0d7f307c8ffe936cb
Arjo Kalisvaart <amplifiedart@gmail.com>
2021-07-12 22:59:15 +0200

    Added Test

---
+++ FrameWorkTests/GenericRangeTest.cs
@@ -0,0 +1,44 @@
+﻿using AA.System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FrameWorkTests
+{
+	[TestClass]
+	public class GenericRangeTest
+	{
+		[TestMethod]
+		public void RangeCompareTest()
+		{
+			GenericRange<int> firstRange = new GenericRange<int>(5, 10);
+			GenericRange<int> secondRange = new GenericRange<int>(3, 7);
+
+			// The first range is larger then the second range (1)
+			Assert.AreEqual(firstRange.CompareTo(secondRange), 1, "5-10 v.s. 3-7");
+
+			// The second range is smaller then the second range (-1)
+			Assert.AreEqual(secondRange.CompareTo(firstRange), -1, "3-7 vs 5-10");
+
+			secondRange = new GenericRange<int>(5, 10);
+			// The first Range is equal to the second range (0)
+			Assert.AreEqual(firstRange.CompareTo(secondRange), 0, "5-10 vs 5-10");
+
+			secondRange = new GenericRange<int>(3, 10);
+			// the first Range is starting after the second range (-1)
+			Assert.AreEqual(firstRange.CompareTo(secondRange), 1, "5-10 vs 3-10");
+
+			secondRange = new GenericRange<int>(5, 8);
+			// the first Range is starting after the second range (-1)
+			Assert.AreEqual(firstRange.CompareTo(secondRange), 1, "5-10 vs 5-8");
+
+			secondRange = new GenericRange<int>(5, 11);
+			// the first Range is starting after the second range (-1)
+			Assert.AreEqual(firstRange.CompareTo(secondRange), -1, "5-10 vs 5-11");
+
+			secondRange = new GenericRange<int>(6, 8);
+			// the first Range is starting after the second range (-1)
+			Assert.AreEqual(firstRange.CompareTo(secondRange), -1, "5-10 vs 6-8");
+
+		}
+	}
+}
---
+++ FrameWorkTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FrameWorkTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrameWorkTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eac84e38-5d8a-4d07-a28d-0f6ecaba831e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
